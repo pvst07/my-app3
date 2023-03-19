7eea733718d60d77808ee9497345ce6d5673e62c
psvt07 <pvs.tw07@gmail.com>
2023-03-19 22:49:58 +0700

    https://github.com/pvst07/my-app3.git

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
 
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";                                         
+         
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
